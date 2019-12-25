7ec3d339e0fbf8f5e50301348c0d885f328aedd9
5w891 <58629942+wh891@users.noreply.github.com>
2019-12-25 10:58:47 +0800

    Add files via upload

---
+++ 2script_terbaru.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+
+clear
+
+ulang="y"
+while [ $ulang = "y" ];
+do
+toilet "untuk username dan password sc 68 adalah username : Solo password : Player" | lolcat
+figlet "5W WE" | lolcat
+echo "______________________by 5W WE"
+  echo "(_= 1 -+] install red hawk"
+  echo "[+- 2 =_# install sc 68"
+  echo "(_= 5 -+] exit"
+read -p "masukan yg kamu mau : " pilih
+if [ $pilih = "1" ];
+then
+        echo "installing red hawk"
+        cd $HOME
+        pkg update && pkg upgrade
+        pkg install git
+        pkg install php
+        git clone https://github.com/Tuhinshubhra/RED_HAWK
+        echo "susses install"
+        sleep 2
+elif [ $pilih = "2" ];
+then
+        echo "installing sc 68"
+        cd $HOME
+        pkg update && pkg upgrade
+        pkg install python
+        pkg install python2
+        pkg install curl
+        pkg install wc
+        pkg install python2-dev
+        pkg install prov
+        pkg install php
+        pkg install git
+        git clone https://github.com/Rusmana-ID/rus
+        cd rus
+        chmod 777 v2.sh
+        sh v2.sh
+        echo "susses install"
+        sleep 2
+elif [ $pilih = "5" ];
+then
+    echo "dahh selamat mencoba lagi"
+    sleep 3
+    exit
+else
+    echo "salah boy harap masukan kembali yg benar"
+    sleep 3
+    echo $ulang
+ fi
+done
